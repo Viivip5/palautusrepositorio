7d2a8047bd4fd0200221e18f62f2321a01071866
viivi <viivi.pesio@psil.fi>
2026-04-28 16:34:57 +0300

    2.4.

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -5,6 +5,7 @@ const Courses = (props) => {
     <div>
       <Header text={props.title} />
       <Content courses={props.courses} />
+
     </div>
   )
 }
@@ -14,30 +15,41 @@ const Header = (props) => {
     <h1>{props.text}</h1>
   )
 }
-const Content = (props) => {
+
+const Content = ({ courses }) => {
   return (
     <div>
-      {props.courses.map(course => (
-        <div key={course.id}>
-          <h1>{course.name}</h1>
-          {course.parts.map(part => (
-            < Part key={part.id} name={part.name} exercises={part.exercises} />
-          ))}
-        </div>
-      ))}
+      {courses.map(course => {
+        const total = course.parts.reduce(
+          (sum, part) => sum + part.exercises,
+          0
+        )
+
+        return (
+          <div key={course.id}>
+            <h1>{course.name}</h1>
+
+            {course.parts.map(part => (
+              <Part key={part.id} {...part} />
+            ))}
+
+            <b>total of {total} exercises</b>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
 
 const Part = (props) => {
-
   return (
     <div>
       {props.name} {props.exercises}
     </div>
   )
 }
+
 
 const App = () => {
   const title = 'Web development curriculum'
