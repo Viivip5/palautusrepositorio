a5c2f1289c284f43fba2a2d065ad401d93b83004
viivi <viivi.pesio@psil.fi>
2026-04-28 16:55:22 +0300

    2.5.kesken

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,57 +1,7 @@
 import { useState } from 'react'
+import { Courses } from './components/Courses'
 
-const Courses = (props) => {
-  return (
-    <div>
-      <Header text={props.title} />
-      <Content courses={props.courses} />
-
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  )
-}
-
-const Content = ({ courses }) => {
-  return (
-    <div>
-      {courses.map(course => {
-        const total = course.parts.reduce(
-          (sum, part) => sum + part.exercises,
-          0
-        )
-
-        return (
-          <div key={course.id}>
-            <h1>{course.name}</h1>
-
-            {course.parts.map(part => (
-              <Part key={part.id} {...part} />
-            ))}
-
-            <b>total of {total} exercises</b>
-          </div>
-        )
-      })}
-    </div>
-  )
-}
-
-
-const Part = (props) => {
-  return (
-    <div>
-      {props.name} {props.exercises}
-    </div>
-  )
-}
-
-
-const App = () => {
+const App = ({ Courses }) => {
   const title = 'Web development curriculum'
   const courses = [
     {
